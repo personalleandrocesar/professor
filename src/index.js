5ded0794beae588b2c923d35d7aa5d73e125d5b2
professorleandrocesar <professor@leandrocesar.co>
2023-01-11 23:48:16 -0300

    primeiro commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-if (window.location.protocol != "https:") {
-    window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
-}
+//if (window.location.protocol != "https:") {
+//    window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
+//}
 export default{
 
     methods: {
